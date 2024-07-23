5dd3afe0c837bddefbe577dc1afb7631aa0aca40
Kamogelo Mkwanazi <kamopmkwanazi@gmail.com>
2024-07-23 15:37:13 +0200

    second commit

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -7,29 +7,33 @@ function Contact () {
   return (
     <div className='contact'>
       <h3 className='headingCont'>CONTACT</h3>
-      <div className='cust-row'>
+      <div className='container'>
           <p><label id='message'>Drop Me a Message:</label></p>
           <textarea id ='message' name='message' rows='5' cols='50'></textarea>
-            
+
+        <div className='column'>
             <div className='phone'>
               <img className='phoneImg'src={Phone} width='40px'></img>
               <p>072 563 1254</p>
-            </div>
 
-            <div className='email'>
-              <img className='emailImg'src={Email} width='40px'></img>
-              <p>kamo@gmail.com</p>
-            </div>
+                  <div className='email'>
+                    <img className='emailImg'src={Email} width='40px'></img>
+                    <p>kamo@gmail.com</p>
 
-            <div className='location'>
-              <img className='locationImg'src={Location} width='40px'></img>
-              <p>123 First street</p>
-            </div>
-
-            <form className='sendMessage'>
-              <input type='text'></input>
-            </form>
+                      <div className='location'>
+                        <img className='locationImg'src={Location} width='40px'></img>
+                        <p>123 First street</p>
+                      
+                </div>
+              </div>
+            </div> 
         </div>
+            <div className='column2'>
+                <form className='sendMessage'>
+                  <input type='text'></input>
+                </form> 
+            </div> 
+      </div>
 
     </div>
       
